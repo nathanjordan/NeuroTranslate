c419ff81afa5f67f1497dfcd221f22e54d27f5d4
Nathan Jordan <natedagreat27274@gmail.com>
2012-04-25 15:21:53 -0700

    fixes

--- src/unr/neurotranslate/ncs/Synapse.java
+++ src/unr/neurotranslate/ncs/Synapse.java
@@ -71,7 +71,15 @@ public class Synapse extends TypedElement {
 			
 			}
 		
-		s = NCSWriter.writeProperty("PREV_SPIKE_RANGE", prevSpikeRange, s);
+		if( prevSpikeRange != null ) {
+			
+			s = s + "\tPREV_SPIKE_RANGE\t";
+			
+			s = s + prevSpikeRange[0] + "\t";
+			
+			s = s + prevSpikeRange[1] + "\n";
+			
+			}
 		
 		s = NCSWriter.writeProperty("MAX_CONDUCT", maxConduct, s);
 		
